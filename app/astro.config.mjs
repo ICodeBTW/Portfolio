460ac73d7cbe2697298f3622d90b59e4ecf42620
Shree <shreenidhihk@outlook.com>
2025-05-31 23:09:47 +1000

    Updated adapter settings and server setting

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 4321, host: "0.0.0.0"},
-  output: 'static',
+  output: 'server',
+  adapter:  awsAmplify(),
   site: "https://www.shreenidhi.dev/",
 
   vite: {
